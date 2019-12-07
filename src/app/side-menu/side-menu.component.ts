c4530fac83c3f2caf9069f8a9f27296e619e7b91
Arsh <arshwaraich12@gmail.com>
2019-12-06 23:32:52 -0500

    Fixed router issue

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class SideMenuComponent implements OnInit {
 
   constructor(
-    private router: Router
+    public router: Router
   ) { }
 
   ngOnInit() {
